655bb69c667cb282f4ce07ee574a8881937a10df
Julien DURIS <julien.duris@radiofrance.com>
2017-12-19 13:45:13 +0100

    Added browser behavior handler

---
+++ src/browser.js
@@ -0,0 +1,51 @@
+var Browser = function()
+{
+    this.focus = [];
+    this.blur = [];
+    this.documentVisibility = {
+        hidden: [],
+        visible: []
+    };
+
+    this.handleBehavior();
+}
+
+Browser.prototype.handleBehavior = function()
+{
+    var self = this;
+
+    window.onfocus = function(e) {self.focus.forEach(function(item) {item(e);});};
+    window.onblur = function(e) {self.blur.forEach(function(item) {item(e);});};
+    document.addEventListener('visibilitychange', function(e) {
+        if (document.visibilityState == "hidden") {
+            self.documentVisibility.hidden.forEach(function(item) {item(e);});
+            return ;
+        }
+
+        self.documentVisibility.visible.forEach(function(item) {item(e);});
+    });
+}
+
+Browser.prototype.onFocus = function(cb)
+{
+    this.focus.push(cb);
+    return this;
+}
+
+Browser.prototype.onBlur = function(cb)
+{
+    this.blur.push(cb);
+    return this;
+}
+
+Browser.prototype.onDocumentHidden = function(cb)
+{
+    this.documentVisibility.hidden.push(cb);
+    return this;    
+}
+
+Browser.prototype.onDocumentVisible = function(cb)
+{
+    this.documentVisibility.visible.push(cb);
+    return this;    
+}
