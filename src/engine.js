38871a25ef3f0e339fb90ae5bffb2c07a83345a9
monkeydioude <monkeydioude@gmail.com>
2018-01-29 13:38:55 +0100

    Add canvas clearing before each reload

--- src/engine.js
+++ src/engine.js
@@ -12,3 +12,12 @@ Engine.prototype.draw = function(x, y, w, h, color) {
 Engine.prototype.clearCanvas = function() {
     this.c.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
+
+Engine.prototype.drawImageData = function(imgData, w, h, x, y, dx, dy) {
+    console.log(imgData);
+    if (!x) x = 0;
+    if (!y) y = 0;
+    if (!dx) dx = 0;
+    if (!dy) dy = 0;
+    this.c.putImageData(imgData, x, y, dx, dy, w, h);
+}
