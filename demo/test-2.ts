789b5f5751bded0453a899eb2a0be95286c994ed
monkeydioude <monkeydioude@gmail.com>
2018-12-12 22:57:17 +0100

    separated responsabilities from loop, add stateMachine centric mechanic, not sure about how I made it tho + pause

--- demo/test-2.ts
+++ demo/test-2.ts
@@ -8,112 +8,58 @@ import {Coordinates} from "zizo/coordinates"
 import {Map} from "zizo/map"
 import {Isometric} from "zizo/isometric"
 import {Loader} from "gloop/assets/loader"
-import { Frameset } from "../../gloop/assets/frameset";
+import {Frameset} from "gloop/assets/frameset"
+import {Updater} from "gloop/updater/updater"
+import {DisplayUpdater} from "gloop/updater/displayUpdater"
+import {StateMachine} from "gloop/stateMachine"
+import {Core} from "zizo/core"
 
-(new Browser()).onReady(function() {
-    let camera = new Camera(new Coordinates(
-            6.5,
-            6.5,
-            config.canvasMX,
-            config.canvasMY,
-            config.tileTopW,
-            config.isoDecalX,
-            config.isoDecalY
-        )),
-        renderer = new Renderer(
-            new Canvas(document.querySelector("#board")),
-            new Canvas(document.querySelector('#buffer'))
-        ),
-        loop = new Loop(30, renderer),
-        engine = new Isometric(
-            renderer,
-            loop.displayUpdater,
-            loop.dataUpdater,
-            camera
-        ),
-        assets = new Loader("../assets");
-    
-    fetch(new Request('../assets/assets.json')).then(function(res) {
-        res.json().then(function(data) {
-            var err = assets.load(data, function(asset: any) {
-                console.log("Just loaded " + asset.name);
-            });
-            if (err != null) {
-                console.log(err.error());
-            }
-        });
-    });
-    // console.log(new Coordinates(64, 32, 16, 0, 0, 0, 0).fromTileCoordinates(3, 0))
+function loadingBehavior(core: Core, state: StateMachine): void {
+    core.getDataUpdater().add("LOADING", () => {
+        if (core.getAssetsLoader().hasFinishedLoading() == true) {
+            state.setState("PLAY")
+            return -1
+        }
+    })
+}
 
-    var map = new Map([
-            ['0_0', '0_0', '0_1', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_1', '0_0', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
-            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
-            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ]
-        ],
-        assets
-    );
+function playBehavior(core: Core, state: StateMachine): void {
+    let camera = core.getDisplayEngine().getCamera(),
+        originalBois = [
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+        core.getAssetsLoader().copy("rebot-sprite"),
+    ];
 
-    engine.setMap(map);
-    engine.start();
-
-    assets.onLoaded(() => map.loadMap());
-
-    loop.setMode("PLAY");
-    loop.addStartingConditions([
-        assets.hasFinishedLoading.bind(assets)
-    ]);
-
-    loop.start();
-
-    let originalBois = [
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-        assets.copy("rebot-sprite"),
-    ];        
-    engine.objectUpdater.add("PLAY", function(T: number, objects: any) {
-        objects.add(assets.get("building1"), 1, 3, 10);
-        objects.add(assets.get("building1"), 3, 8, 10);
-        objects.add(assets.get("building1"), 6, 2, 10);
-        objects.add(assets.get("building1"), 14, 7, 10);
-        objects.add(assets.get("building1"), 9, 14, 10);
-        objects.add(assets.get("building1"), 10, 10, 10);
+    core.getDisplayEngine().objectUpdater.add("PLAY", function(T: number, objects: any) {
+        objects.add(core.getAssetsLoader().get("building1"), 1, 3, 10);
+        objects.add(core.getAssetsLoader().get("building1"), 3, 8, 10);
+        objects.add(core.getAssetsLoader().get("building1"), 6, 2, 10);
+        objects.add(core.getAssetsLoader().get("building1"), 14, 7, 10);
+        objects.add(core.getAssetsLoader().get("building1"), 9, 14, 10);
+        objects.add(core.getAssetsLoader().get("building1"), 10, 10, 10);
         objects.add(originalBois[0], 2, 7)
-        // objects.add(originalBois[1], 6.5, 6.5)
-        // objects.add(originalBois[2], 6.5, 6.75)
-        // objects.add(originalBois[3], 6.5, 7)
-        // objects.add(originalBois[4], 6.5, 7.25)
-        // objects.add(originalBois[5], 6.5, 7.5)
-        // objects.add(originalBois[6], 6.5, 7.75)
-        // objects.add(originalBois[7], 6.5, 8)
-        // objects.add(originalBois[8], 6.5, 8.25)
-        // objects.add(originalBois[9], 6.5, 8.5)
+        objects.add(originalBois[1], 6.5, 6.5)
+        objects.add(originalBois[2], 6.5, 6.75)
+        objects.add(originalBois[3], 6.5, 7)
+        objects.add(originalBois[4], 6.5, 7.25)
+        objects.add(originalBois[5], 6.5, 7.5)
+        objects.add(originalBois[6], 6.5, 7.75)
+        objects.add(originalBois[7], 6.5, 8)
+        objects.add(originalBois[8], 6.5, 8.25)
+        objects.add(originalBois[9], 6.5, 8.5)
     }, "buildings")
 
-    loop.displayUpdater.add("PLAY", function() {
+    core.getDisplayUpdater().add("PLAY", function() {
         var arrowSize = 8;
-        renderer.drawLine(400, (config.canvasH / 2) - (arrowSize / 2), 400, (config.canvasH / 2) + (arrowSize / 2))
-        renderer.drawLine((config.canvasW / 2) - (arrowSize / 2), 300, (config.canvasW / 2) + (arrowSize / 2), 300)
+        core.getDisplayEngine().renderer.drawLine(400, (config.canvasH / 2) - (arrowSize / 2), 400, (config.canvasH / 2) + (arrowSize / 2))
+        core.getDisplayEngine().renderer.drawLine((config.canvasW / 2) - (arrowSize / 2), 300, (config.canvasW / 2) + (arrowSize / 2), 300)
     }, "camera");
 
     var cameraTileMove = 0.25;
@@ -150,8 +96,8 @@ import { Frameset } from "../../gloop/assets/frameset";
                 buildings[label].rv = -1
             }
 
-            engine.objectUpdater.add("PLAY", function(T: number, objects: any) {
-                objects.add(assets.get("building1"), buildings[label].x << 0, buildings[label].y << 0, 10)
+            core.getDisplayEngine().objectUpdater.add("PLAY", function(T: number, objects: any) {
+                objects.add(core.getAssetsLoader().get("building1"), buildings[label].x << 0, buildings[label].y << 0, 10)
                 let rv = buildings[label].rv;
                 if (rv == -1) {
                     delete buildings[label];
@@ -162,7 +108,7 @@ import { Frameset } from "../../gloop/assets/frameset";
         "c": () => {
             let clabel = "rebot-" + (camera.coord.icX) + (camera.coord.icY);
             if (!anims.hasOwnProperty(clabel)){
-                var e = <Frameset> assets.copy("rebot-sprite"),
+                var e = <Frameset> core.getAssetsLoader().copy("rebot-sprite"),
                 c = new Coordinates(
                         0,
                         0,
@@ -183,7 +129,7 @@ import { Frameset } from "../../gloop/assets/frameset";
             } else {
                 anims[clabel].rv = -1
             }
-            engine.objectUpdater.add("PLAY", function(T: number, objects: any) {
+            core.getDisplayEngine().objectUpdater.add("PLAY", function(T: number, objects: any) {
                 objects.add(anims[clabel].entity, anims[clabel].x, anims[clabel].y, 0);
                 let rv = anims[clabel].rv;
                 if (rv == -1) {
@@ -191,12 +137,109 @@ import { Frameset } from "../../gloop/assets/frameset";
                 }
                 return rv
             }, clabel)
-                // objects.add(assets.get("rebot-sprite"), buildings[label].x << 0, buildings[label].y << 0);
+                // objects.add(core.getAssetsLoader().get("rebot-sprite"), buildings[label].x << 0, buildings[label].y << 0);
+        },
+        "p": (e: any) => {
+            state.setState("PAUSE")
+            document.removeEventListener("keydown", listener)
         }
     }
-    document.addEventListener("keydown", function(e) {
-        if (pressFunc.hasOwnProperty(e.key)) {
-            pressFunc[e.key]();
+    let listener = function(e: any) {
+        if (pressFunc.hasOwnProperty(e.key) && state.getState() == "PLAY") {
+            pressFunc[e.key](e);
         }
+    }
+    document.addEventListener("keydown", listener);
+}
+
+function pauseBehavior(core: Core, state: StateMachine): void {
+    core.getDisplayEngine().getRenderer().scene.c.fillStyle = "rgba(245, 124, 0, 0.5)"
+    core.getDisplayEngine().getRenderer().scene.c.fillRect(0, 0, config.canvasW, config.canvasH)
+    let pressFunc: any = {
+        "p": function(listener: any){
+            state.setState("PLAY")
+            document.removeEventListener("keydown", listener)
+        }
+    },
+    listener = (e: any) => {
+        if (pressFunc.hasOwnProperty(e.key) && state.getState() == "PAUSE") {
+            pressFunc[e.key](this);
+        }
+    };
+    document.addEventListener("keydown", listener)
+}
+
+(new Browser()).onReady(function() {
+    let camera = new Camera(new Coordinates(
+            6.5,
+            6.5,
+            config.canvasMX,
+            config.canvasMY,
+            config.tileTopW,
+            config.isoDecalX,
+            config.isoDecalY
+        )),
+        renderer = new Renderer(
+            new Canvas(document.querySelector("#board")),
+            new Canvas(document.querySelector('#buffer'))
+        ),
+        displayUpdater = new DisplayUpdater(renderer),
+        dataUpdater = new Updater(),
+        assets = new Loader("../assets"),
+        engine = new Isometric(
+            renderer,
+            displayUpdater,
+            dataUpdater,
+            camera
+            ),
+        core = new Core(displayUpdater, dataUpdater, assets, engine),
+        sm = new StateMachine(core),
+        loop = new Loop(30, sm, displayUpdater, dataUpdater);
+    
+    fetch(new Request('../assets/assets.json')).then(function(res) {
+        res.json().then(function(data) {
+            var err = assets.load(data, function(asset: any) {
+                console.log("Just loaded " + asset.name);
+            });
+            if (err != null) {
+                console.log(err.error());
+            }
+        });
     });
+    // console.log(new Coordinates(64, 32, 16, 0, 0, 0, 0).fromTileCoordinates(3, 0))
+
+    var map = new Map([
+            ['0_0', '0_0', '0_1', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_1', '0_0', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', null, null, null, '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', null, null, null, '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ],
+            ['0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1' ],
+            ['0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0', '0_1', '0_0' ]
+        ],
+        assets
+    );
+
+    core.getDisplayEngine().setMap(map);
+    core.getDisplayEngine().start();
+
+    core.getAssetsLoader().onLoaded(() => map.loadMap());
+    loop.start();
+
+    sm.addStateBehavior("LOADING", loadingBehavior)
+    sm.addStateBehavior("PLAY", playBehavior)
+    sm.addStateBehavior("PAUSE", pauseBehavior)
+
+    sm.setState("LOADING");
 });
